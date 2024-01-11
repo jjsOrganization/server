66d7e84eb7051884d4fe00ef81299a49160b105d
kimkyungho <sgn07124@gmail.com>
2024-01-12 01:15:09 +0900

    ADD JPA entities files

--- src/main/java/com/jjs/ClothingInventorySaleReformPlatform/ClothingInventorySaleReformPlatformApplication.java
+++ src/main/java/com/jjs/ClothingInventorySaleReformPlatform/ClothingInventorySaleReformPlatformApplication.java
@@ -8,6 +8,7 @@ public class ClothingInventorySaleReformPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClothingInventorySaleReformPlatformApplication.class, args);
+
 	}
 
 }
