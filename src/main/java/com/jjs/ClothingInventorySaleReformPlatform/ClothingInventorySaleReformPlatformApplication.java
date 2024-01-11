19f7d689ea8dfe96c24338b0356f3a739a0db7d8
kimkyungho <sgn07124@gmail.com>
2024-01-11 17:31:09 +0900

    Initial commit

---
+++ src/main/java/com/jjs/ClothingInventorySaleReformPlatform/ClothingInventorySaleReformPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jjs.ClothingInventorySaleReformPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingInventorySaleReformPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingInventorySaleReformPlatformApplication.class, args);
+	}
+
+}
