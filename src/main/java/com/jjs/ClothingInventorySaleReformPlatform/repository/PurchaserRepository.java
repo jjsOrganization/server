8cfbf4d81b05fead2284f0bf970885b78ec28121
kimkyungho <sgn07124@gmail.com>
2024-01-19 04:25:58 +0900

    회원가입, 로그인 구현 완료 및 PurchaserInfo, User 테이블에 각각 저장 완료 Seller, Designer에도 추가, 설정 및 엔티티 수정 필요

--- src/main/java/com/jjs/ClothingInventorySaleReformPlatform/repository/PurchaserRepository.java
+++ src/main/java/com/jjs/ClothingInventorySaleReformPlatform/repository/PurchaserRepository.java
@@ -1,20 +1,7 @@
 package com.jjs.ClothingInventorySaleReformPlatform.repository;
 
-import com.jjs.ClothingInventorySaleReformPlatform.domain.User;
+import com.jjs.ClothingInventorySaleReformPlatform.domain.PurchaserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PurchaserRepository extends JpaRepository<User, String> {
-
-    Boolean existsByEmail(String email);
-
-    // email을 받아 DB 테이블에서 회원을 조회하는 메소드
-    User findByEmail(String email);
-
-
-    /*
-    Optional<Purchaser> findByEmail(String email);
-    Optional<Purchaser> findByPhoneNumber(String phonenumber);
-    Optional<Purchaser> findByNickname(String nickname);
-
-     */
+public interface PurchaserRepository extends JpaRepository<PurchaserInfo, String> {
 }
---
+++ src/main/java/com/jjs/ClothingInventorySaleReformPlatform/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.jjs.ClothingInventorySaleReformPlatform.repository;
+
+import com.jjs.ClothingInventorySaleReformPlatform.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Boolean existsByEmail(String email);
+
+    // email을 받아 DB 테이블에서 회원을 조회하는 메소드
+    User findByEmail(String email);
+
+
+    /*
+    Optional<Purchaser> findByEmail(String email);
+    Optional<Purchaser> findByPhoneNumber(String phonenumber);
+    Optional<Purchaser> findByNickname(String nickname);
+
+     */
+}
