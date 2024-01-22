d071af37613b0ba802d316775b46e7118b4db829
kimkyungho <sgn07124@gmail.com>
2024-01-22 15:42:12 +0900

    로그인, 로그인 테스트 성공/실패 시 응답 body 수정

--- src/main/java/com/jjs/ClothingInventorySaleReformPlatform/controller/UserController.java
+++ src/main/java/com/jjs/ClothingInventorySaleReformPlatform/controller/UserController.java
@@ -6,7 +6,10 @@ import com.jjs.ClothingInventorySaleReformPlatform.error.ErrorCode;
 import com.jjs.ClothingInventorySaleReformPlatform.error.ErrorResponse;
 import com.jjs.ClothingInventorySaleReformPlatform.error.exception.CustomValidationException;
 import com.jjs.ClothingInventorySaleReformPlatform.jwt.dto.TokenDto;
+import com.jjs.ClothingInventorySaleReformPlatform.jwt.provider.JwtTokenProvider;
+import com.jjs.ClothingInventorySaleReformPlatform.response.AuthResponseDTO;
 import com.jjs.ClothingInventorySaleReformPlatform.response.ResultResponse;
+import com.jjs.ClothingInventorySaleReformPlatform.service.CustomUserDetailsService;
 import com.jjs.ClothingInventorySaleReformPlatform.service.UserService;
 import com.jjs.ClothingInventorySaleReformPlatform.response.AuthResultCode;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +17,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -32,60 +39,57 @@ import java.util.stream.Collectors;
 public class UserController {
 
     private final UserService userService;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    /*  // 로그인 JSON 방식 요청했을 때
-    @PostMapping("/auth/login")
-    public TokenDto login(@RequestBody UserLoginRequestDto memberLoginRequestDto) {
-        String memberId = memberLoginRequestDto.getMemberId();
-        String password = memberLoginRequestDto.getPassword();
-        TokenDto tokenDto = userService.login(memberId, password);
-        return tokenDto;
-    }
-     */
 
-    // 로그인 JSON 방식 또는 FormData 형식 처리
     @PostMapping("/auth/login")
-    public TokenDto login(HttpServletRequest request) {
+    public ResponseEntity<Object> login(@RequestBody UserLoginRequestDto memberLoginRequestDto) {
         try {
-            UserLoginRequestDto memberLoginRequestDto;
-            String contentType = request.getContentType();
-
-            if (contentType != null && contentType.contains("application/json")) {
-                // JSON 형식의 요청 처리
-                ObjectMapper mapper = new ObjectMapper();
-                memberLoginRequestDto = mapper.readValue(request.getInputStream(), UserLoginRequestDto.class);
-            } else {
-                // FormData 형식의 요청 처리
-                MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-                String memberId = multipartRequest.getParameter("memberId");
-                String password = multipartRequest.getParameter("password");
-                // 여기에서 필요한 경우 파일 처리도 할 수 있습니다.
-                // MultipartFile file = multipartRequest.getFile("file");
-
-                memberLoginRequestDto = new UserLoginRequestDto();
-                memberLoginRequestDto.setMemberId(memberId);
-                memberLoginRequestDto.setPassword(password);
-            }
-
-            // 로그인 처리
             String memberId = memberLoginRequestDto.getMemberId();
             String password = memberLoginRequestDto.getPassword();
             TokenDto tokenDto = userService.login(memberId, password);
-            return tokenDto;
-        } catch (Exception e) {
-            throw new RuntimeException("로그인 요청 처리 중 오류가 발생했습니다.", e);
+
+            ResultResponse resultResponse = ResultResponse.of(
+                    AuthResultCode.LOGIN_SUCCESS, // 로그인 성공 코드
+                    tokenDto // 로그인 성공 시 반환되는 토큰 정보
+            );
+            return new ResponseEntity<>(resultResponse, HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            ErrorResponse errorResponse = ErrorResponse.of(
+                    ErrorCode.AUTHENTICATION_NOT_FOUND, // 로그인 실패 코드
+                    e.getMessage() // 실패 메시지
+            );
+            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
         }
     }
 
     /**
      * 로그인 후 accessToken 검증하는 용도 - 로그로 확인
-     * (로그인 후 accessToken 복사해서 http://localhost:8080/auth/login-test 경로의 Headers의 Authorization에 Bearer+공백+accessToken(+포함x) 붙여넣고 실행)
+     * (로그인 후 accessToken 복사해서 http://localhost:8080/auth/login-test 경로의 Headers의 Authorization에 accessToken 붙여넣고 실행)
      * 만약 accessToken일 일치하지 않으면 JWT signature does not match locally computed signature. JWT validity cannot be asserted and should not be trusted.
      * 일치하지 않는다는 에러 발생
       */
     @PostMapping("/auth/login-test")
-    public String test() {
-        return "success";
+    public ResponseEntity<Object> test(@RequestHeader(value = "Authorization", required = false) String tokenHeader) {
+        if (tokenHeader != null && jwtTokenProvider.validateToken(tokenHeader)) {
+            // 토큰 검증 성공
+            String successMessage = "success";
+            ResultResponse successResponse = ResultResponse.of(
+                    AuthResultCode.LOGIN_SUCCESS, // 접근 성공 코드
+                    successMessage // 성공 메시지
+            );
+            return ResponseEntity.ok(successResponse);
+        } else {
+            // 토큰 검증 실패 또는 토큰 누락
+            String failMessage = "Access Denied: Invalid or missing token.";
+            ErrorResponse failResponse = ErrorResponse.of(
+                    ErrorCode.AUTHENTICATION_NOT_FOUND, // 접근 실패 코드
+                    failMessage // 실패 메시지
+            );
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(failResponse);
+        }
     }
 
     // 구매자 회원가입
